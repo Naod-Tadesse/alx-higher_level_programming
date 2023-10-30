36969ef62e90a3d106874e03ebb878fe7bdbb0cd
Naod Taddese <naodtds@gmail.com>
2023-10-30 22:21:28 +0300

    cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,5 +2,31 @@
 
 
 /**
+ * check_cycle - checker function if a singly linked list has cycle
+ * @list: pointer to first node
+ * Return: return 1 if it is circular, 0 other way
+ */
 
-*/
+int check_cycle(listint_t *list)
+{
+	listint_t *present, *next_element;
+
+	if (list == NULL)
+	{
+		return (0);
+	}
+
+	present = list;
+	next_element = list->next;
+
+	while (present && next_element && next_element->next)
+	{
+		if (present == next_element)
+		{
+			return (1);
+		}
+		present = present->next;
+		next_element = next_element->next->next;
+	}
+	return (0);
+}
