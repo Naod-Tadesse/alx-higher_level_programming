e0699f5d6d1ddd502d4cdc38be02359c029d61ed
Naod-Tadesse <naodtds@gmail.com>
2023-10-31 14:53:44 -0700

    styling fix

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,11 +3,9 @@ def uppercase(str):
     string = ''
     for let in str:
         ascii_rep = ord(let)
-        if ascii_rep >=  97 and ascii_rep <= 122:
+        if ascii_rep >= 97 and ascii_rep <= 122:
             let = chr(ascii_rep - 32)
             string = string + let
         else:
             string = string + let
     print("{}".format(string))
-
-
