751ac32211bd718405abb4d0fd825946ffb338c9
Naod-Tadesse <naodtds@gmail.com>
2023-10-31 14:40:18 -0700

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    string = ''
+    for let in str:
+        ascii_rep = ord(let)
+        if ascii_rep >=  97 and ascii_rep <= 122:
+            letter = chr(ascii_rep - 32)
+            string = string + letter
+    print("{}".format(string))
+
+
