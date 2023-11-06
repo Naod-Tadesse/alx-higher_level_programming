d64385bf1b2f6511c00733ce1b47b42f5ffb076b
Naod Taddese <naodtds@gmail.com>
2023-11-06 10:18:36 +0300

    task 10 fix function definition

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def divisible_by_12(my_list=[]):
+def divisible_by_2(my_list=[]):
     new_list=[]
     for i in range(len(my_list)):
         if (my_list[i] % 2) == 0:
-            new_list += True
+            new_list += [True]
         else:
-            new_list += False
+            new_list += [False]
     return new_list
