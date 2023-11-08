ce95544c6cb7bdbeabbabf2651cf71bedc25476b
Naod Taddese <naodtds@gmail.com>
2023-11-08 21:20:56 +0300

    task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary:
+        alist = list(a_dictionary.keys())
+        score = 0
+        best = ""
+        for  i in alist:
+            if a_dictionary[i] > score:
+                score = a_dictionary[i]
+                best = i
+        return best
