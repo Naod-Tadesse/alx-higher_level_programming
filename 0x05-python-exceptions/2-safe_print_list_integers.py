703d96b2d3e261869ce60cd76923f78e84fe2df4
Naod Taddese <naodtds@gmail.com>
2023-11-20 23:49:23 +0300

    task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    element = 0
+    try:
+        for i in range(x):
+            try:
+                print("{:d}".format(my_list[i]), end="")
+                element = element + 1
+            except (ValueError, TypeError):
+                pass
+        print()
+    except ZeroDivisionError:
+        pass
+    return element
