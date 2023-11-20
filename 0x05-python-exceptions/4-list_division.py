2d9bf1f179fe5940b5833b59d37a2c4da72270a2
Naod Taddese <naodtds@gmail.com>
2023-11-21 01:17:34 +0300

    1

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
+    """this function handles division betwee two lists"""
     li = []
     result = 0
     for i in range(list_length):
+        result = 0
         try:
             result = my_list_1[i] / my_list_2[i]
         except IndexError:
