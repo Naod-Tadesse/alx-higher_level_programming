bdb57677f9ad3c2078b46a9915554aae0c187ee8
Naod Taddese <naodtds@gmail.com>
2023-11-21 22:01:12 +0300

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+"""square class definition"""
+
+
+class Square:
+    """square"""
+
+    def __init__(self, size):
+        """
+        initialize the private __size with size
+        """
+        self.__size = size
