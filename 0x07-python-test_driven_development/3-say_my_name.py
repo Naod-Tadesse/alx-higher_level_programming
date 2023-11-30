8fd556f8780d22d8b500433b2b4bf8bc35b0b97e
Naod Taddese <naodtds@gmail.com>
2023-11-30 13:59:47 +0300

    fix

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,7 +4,7 @@
 
 def say_my_name(first_name, last_name=""):
     """this is a function that prints name"""
-    if type(last_name) != str:
+    if not (type(last_name) == str):
         raise TypeError("last_name must be a string")
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
