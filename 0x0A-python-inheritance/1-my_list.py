93f87cfe3cb479c408dc0e2108bd92b547d26e33
Naod Taddese <naodtds@gmail.com>
2023-12-04 21:46:39 +0300

    task 1 fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,6 +4,11 @@
 
 class MyList(list):
     """this inherits from list"""
+
+    def __init__(self):
+        """instantiate"""
+        super().__init__()
+
     def print_sorted(self):
         """this method returns sorted list"""
         print(sorted(self))
