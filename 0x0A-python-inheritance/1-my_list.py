3447b794b8df34ca32bf06920285432e829ddee9
Naod Taddese <naodtds@gmail.com>
2023-12-04 21:40:06 +0300

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+"""this class inherits from list class"""
+
+
+class MyList(list):
+    """this inherits from list"""
+    def print_sorted(self):
+        """this method returns sorted list"""
+        print(sorted(self))
