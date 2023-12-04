226d80e1128a6dfa5ff988cd66b3a6577cf87d93
Naod Taddese <naodtds@gmail.com>
2023-12-04 22:40:24 +0300

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""this fun return true if object is instance of given class """
+
+
+def is_same_class(obj, a_class):
+    """functjion implementation"""
+    if (type(obj) == a_class):
+        return True
+    else:
+        return False
