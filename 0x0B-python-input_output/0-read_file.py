4b9b513fc38424f913df373d39b04a4fcc61905a
Naod Taddese <naodtds@gmail.com>
2023-12-05 20:18:52 +0300

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""function that reads file"""
+
+def read_file(filename=""):
+    """read file"""
+	with open(filename, encoding="utf-8") as openfile:
+        print(openfile.read(), end="")
