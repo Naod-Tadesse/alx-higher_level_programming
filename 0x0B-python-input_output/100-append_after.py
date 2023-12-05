665213ac0316d7d27139c3ea020bbb30153adc4f
Naod Taddese <naodtds@gmail.com>
2023-12-05 23:23:48 +0300

    100 tasks

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,12 +5,10 @@
 def append_after(filename="", search_string="", new_string=""):
     """body"""
     string = ""
-    with open(filename, 'r') as openfile:
+    with open(filename, 'r') as openfile, open(filename, "w") as openedfile:
         reads = openfile.readlines()
         for everyline in reads:
             string += everyline
             if search_string in everyline:
                 string += new_string
-
-    with open(filename, "w") as openedfile:
         openedfile.write(string)
