2e2342820d3260cd06617bee017167853540d35e
Naod Taddese <naodtds@gmail.com>
2023-12-05 21:47:45 +0300

    tas 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """function that saves all arguments of python to file"""
 
-from sys import argv
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-
-try:
-    the_list = load_from_json_file("add_item.json")
-
-except FileNotFoundError:
-    the_list = []
-the_list.extend(argv[1:])
-save_to_json_file(the_list, "add_item.json")
+import sys
+if __name__ == "__main__":
+    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    try:
+        the_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        the_list = []
+    the_list.extend(sys.argv[1:])
+    save_to_json_file(the_list, "add_item.json")
