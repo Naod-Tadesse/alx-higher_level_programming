a08da159d4d75195a897430ad9cc1a9e79a83d2c
Naod Taddese <naodtds@gmail.com>
2023-12-05 21:30:38 +0300

    task 6 fix

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -4,7 +4,8 @@ load json
 """
 import json
 
+
 def load_from_json_file(filename):
-   """from json file to obj"""
-   with open(filename) as openfile:
-       return json.load(openfile)
+    """from json file to obj"""
+    with open(filename) as openfile:
+        return json.load(openfile)
