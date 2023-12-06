a0931f3b13b889274d5a72fc5852eee258434c28
Naod Taddese <naodtds@gmail.com>
2023-12-06 21:57:29 +0300

    task 100 fix

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -2,13 +2,13 @@
 """blue print class myint"""
 
 
-class MyInt:
+class MyInt(int):
     """implementation"""
 
     def __ne__(self, number):
         """not equal implementation"""
         return not super().__ne__(number)
 
-    def __eq__(self, other):
+    def __eq__(self, number):
         """equal implementation"""
         return not super().__eq__(number)
