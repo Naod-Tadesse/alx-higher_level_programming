4bc27b94c6d75c5d14841d6df5ba02ffbe7415cb
Naod Taddese <naodtds@gmail.com>
2023-12-06 22:30:07 +0300

    finish

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,6 +5,7 @@
 def add_attribute(dictio, attribute_name, value):
     """implementation"""
 
-    if not hasattr(dictio, "__dict__") and not hasattr(type(dictio), "__slots__"):
-        raise TypeError("Can't add new attribute")
-    setattr(dictio, attribute_name, value)
+    if hasattr(dictio, "__dict__"):
+        setattr(dictio, attribute_name, value)
+    else:
+        raise TypeError("can't add new attribute")
