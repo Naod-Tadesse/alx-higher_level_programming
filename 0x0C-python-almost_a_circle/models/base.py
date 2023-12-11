1a5792479f9ce5df9c89130539f585569afba1fb
Naod Taddese <naodtds@gmail.com>
2023-12-11 23:52:02 +0300

    task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,3 +13,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """dict to str"""
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
