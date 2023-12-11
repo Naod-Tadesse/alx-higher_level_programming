3c073ff643c55ca86e2838d8610a68149827a9b2
Naod Taddese <naodtds@gmail.com>
2023-12-11 23:54:12 +0300

    task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """the blueprint class for classes"""
+import json
 
 
 class Base:
@@ -18,5 +19,5 @@ class Base:
     def to_json_string(list_dictionaries):
         """dict to str"""
         if list_dictionaries is None:
-            return "[]"
+            return '[]'
         return json.dumps(list_dictionaries)
