2e441b92aa36c00bcc1979839d4c3316e54dccf8
Naod Taddese <naodtds@gmail.com>
2024-01-17 20:55:54 +0300

    task 102

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,4 +1,4 @@
 -- display top 3 of cities temprature during july
 
-SELECT city, AVG(value) AS avg_temp FROM tempratures
+SELECT city, AVG(value) AS avg_temp FROM temperatures
 WHERE month = 7 OR month=8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
