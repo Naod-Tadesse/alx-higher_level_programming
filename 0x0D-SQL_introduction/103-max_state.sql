ace2bbaa9f22804413f81082cbe4e81e31dde53c
Naod Taddese <naodtds@gmail.com>
2024-01-17 20:56:42 +0300

    task 103

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,3 @@
 -- this displays max temprature of each state
-SELECT state, MAX(value) as max_temp FROM tempratures
+SELECT state, MAX(value) as max_temp FROM temperatures
 GROUP BY state ORDER BY state;
