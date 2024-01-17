a37da03af1aff224adb847553f073277c1d3537c
Naod Taddese <naodtds@gmail.com>
2024-01-17 23:23:21 +0300

    task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,8 @@
 -- script creates msql sever for user
 
 CREATE USER IF NOT EXISTS 
-user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+user_0d_1@localhost IDENTIFIED BY 'user_0d_1';
 GRANT ALL PRIVILEGES
-ON *.* TO 'user_0d_1'@'localhost'
+ON *.* TO 'user_0d_1'@'
 IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
