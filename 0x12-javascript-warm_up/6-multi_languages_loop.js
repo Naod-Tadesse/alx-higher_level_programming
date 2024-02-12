39e8af7774ba3400f5732dcf2508cc0523cf1da0
Naod Taddese <naodtds@gmail.com>
2024-02-12 23:41:48 +0300

    semistandard style for task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const toPrint = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+const toPrint = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (const item of toPrint) {
-	console.log(item);
+  console.log(item);
 }
