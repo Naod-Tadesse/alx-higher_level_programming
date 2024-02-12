3b02451b9e9eb0318665dc9e76fde792a74267fb
Naod Taddese <naodtds@gmail.com>
2024-02-12 23:36:40 +0300

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 
-console.log((process.argv.length === 2) ? 'No argument' : 'Arguments found');
+console.log((process.argv.length === 2) ? 'No argument' : process.argv.length === 3 ? 'Argument found' : 'Arguments found');
