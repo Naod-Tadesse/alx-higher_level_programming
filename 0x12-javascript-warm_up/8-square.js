0e24103aada7652041da218ec0191f61eb0476f5
Naod Taddese <naodtds@gmail.com>
2024-02-12 23:57:05 +0300

    shorter

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,10 +6,6 @@ if (isNaN(num)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < num; i++) {
-    let p = '';
-    for (let k = 0; k < num; k++) {
-      p += 'X';
-    }
-    console.log(p);
+    console.log('X'.repeat(num));
   }
 }
