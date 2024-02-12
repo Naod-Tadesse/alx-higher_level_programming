f420e90a54d97582abd744d92e2dfe749cb96b95
Naod Taddese <naodtds@gmail.com>
2024-02-12 23:48:49 +0300

    task 7 fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
 
-console.log(isNaN(parseInt(process.argv[2])) ? 'Missing number of occurrences' : 'C is fun\n'.repeat(parseInt(process.argv[2]) - 1) + 'C is fun');
+if (isNaN(parseInt(process.argv[2]))) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log('C is fun');
+  }
+}
