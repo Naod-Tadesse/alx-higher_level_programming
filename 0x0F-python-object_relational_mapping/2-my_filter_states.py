cda50773a47c004bfddb4512878f5aab4c90e5c1
Naod Taddese <naodtds@gmail.com>
2024-02-19 11:14:06 +0300

    add binary

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
                           db=argv[3], host='localhost')
     connection = con.cursor()
     n = argv[4]
-    connection.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id"
+    connection.execute("SELECT * FROM states WHERE binary name = '{}' ORDER BY id"
                        .format(n))
     entries = connection.fetchall()
     for entry in entries:
