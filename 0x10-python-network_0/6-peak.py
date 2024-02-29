4194284c9cda390836069966ec74cd98b9196a85
Naod Taddese <naodtds@gmail.com>
2024-02-29 20:05:08 +0300

    task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""the highest of the neighbours"""
+
+
+def find_peak(list_of_integers):
+    """this calculates the peak"""
+    if list_of_integers:
+        lef = 0
+        r = len(list_of_integers) - 1
+
+        while lef < r:
+            m = (lef + r) // 2
+
+            if list_of_integers[m] > list_of_integers[m + 1]:
+                r = m
+            else:
+                lef = m + 1
+        return list_of_integers[lef]
+    else:
+        return None
