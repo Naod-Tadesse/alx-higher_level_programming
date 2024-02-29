16a0900e5fa755e8db763e31f1ef6a598060772b
Naod Taddese <naodtds@gmail.com>
2024-02-29 20:10:25 +0300

    change

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,10 +11,10 @@ def find_peak(list_of_integers):
         while lef < r:
             m = (lef + r) // 2
 
-            if list_of_integers[m] > list_of_integers[m + 1]:
-                r = m
-            else:
+            if list_of_integers[m] < list_of_integers[m + 1]:
                 lef = m + 1
+            else:
+                r = m
         return list_of_integers[lef]
     else:
         return None
