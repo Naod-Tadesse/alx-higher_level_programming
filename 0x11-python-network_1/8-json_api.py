d368d3a5882dc5d5ff5de95f9c0e3ddaf1d638c0
Naod Taddese <naodtds@gmail.com>
2024-03-05 22:00:49 +0300

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" search for user using q as param"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    link = "http://0.0.0.0:5000/search_user"
+    if len(argv) == 1:
+        dat = {"q": ""}
+    else:
+        data = {"q": argv[1]}
+
+    result = requests.post(link, data=dat)
+    try:
+        resp = result.json()
+        if not (res == {}):
+            print(f"[{resp.get("id")}] {resp.get("name")}")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
