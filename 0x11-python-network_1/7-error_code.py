e66299f927a2fc199cac86fe8ea63c0493c71b38
Naod Taddese <naodtds@gmail.com>
2024-03-05 21:48:23 +0300

    task 7 fix

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,7 +6,7 @@ import requests
 if __name__ == "__main__":
 
     resp = requests.get(argv[1])
-    if resp.status_code >= 400):
+    if resp.status_code >= 400:
         print(f"Error code: {resp.status_code}")
     else:
         print(resp.text)
