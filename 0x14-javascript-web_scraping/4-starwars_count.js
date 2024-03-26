876b3fcd68d57c7ff1f588afeea6d9a2e7824be2
Naod Taddese <naodtds@gmail.com>
2024-03-26 19:44:23 +0300

    fix checker error

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,14 +4,16 @@ const request = require('request');
 const apiUrl = process.argv[2];
 
 request.get(apiUrl, (error, response, body) => {
-  if (!error) {
-    if (response.statusCode === 200) {
-      const films = JSON.parse(body).results;
-      const moviesWithWedgeAntilles = films.filter(film =>
-        film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
-      );
-      console.log(moviesWithWedgeAntilles.length);
-    }
+  if (error) {
+    console.error(error);
+  }
+
+  if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    const moviesWithWedgeAntilles = films.filter(film =>
+      film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
+    );
+    console.log(moviesWithWedgeAntilles.length);
   } else {
     console.error(error);
   }
