b2c434f91ff3802b7b24d72374930720a57c4f79
Naod Taddese <naodtds@gmail.com>
2024-03-26 21:42:31 +0300

    finish

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -9,37 +9,25 @@ request.get(apiUrl, (error, response, body) => {
     console.error(error);
     return;
   }
+  const film = JSON.parse(body);
+  const characters = film.characters;
 
-  if (response.statusCode !== 200) {
-    console.error(`Failed to fetch movie data: ${response.statusCode}`);
-    return;
-  }
-
-  const movie = JSON.parse(body);
-  const characters = movie.characters;
-
-  const printCharacters = (index) => {
+  const getChar = (index) => {
     if (index === characters.length) {
       return;
     }
 
     const characterUrl = characters[index];
-    request.get(characterUrl, (charError, charResponse, charBody) => {
-      if (charError) {
-        console.error(charError);
-        return;
+    request.get(characterUrl, (cError, cResponse, cBody) => {
+      if (cError) {
+        console.error(cError);
       }
 
-      if (charResponse.statusCode !== 200) {
-        console.error(`Failed to fetch character data: ${charResponse.statusCode}`);
-        return;
-      }
-
-      const character = JSON.parse(charBody);
+      const character = JSON.parse(cBody);
       console.log(character.name);
-      printCharacters(index + 1);
+      getChar(index + 1);
     });
   };
 
-  printCharacters(0);
+  getChar(0);
 });
