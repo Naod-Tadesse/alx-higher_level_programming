2832499155c051c42e2f0729322ed08b4505717b
Naod Taddese <naodtds@gmail.com>
2024-03-26 18:49:13 +0300

    fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 
-request(process.argv[2], (error, response) => {
+request.get(process.argv[2], (error, response) => {
   if (error) {
     console.error(error);
   } else {
