7504f47a8d03c09f1588bef094d560977c660eb0
Naod Taddese <naodtds@gmail.com>
2024-03-26 19:30:48 +0300

    fixes

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,23 +2,12 @@
 const request = require('request');
 
 const apiUrl = process.argv[2];
-const characterId = '18'; // Wedge Antilles character ID
 
 request.get(apiUrl, (error, response, body) => {
-  if (error) {
+  if (!error && response.statusCode === 200) {
+    const count = body.split('people/18').length - 1;
+    console.log(count);
+  } else {
     console.error(error);
-    return;
   }
-
-  if (response.statusCode !== 200) {
-    console.error(`Failed to fetch movie data: ${response.statusCode}`);
-    return;
-  }
-
-  const films = JSON.parse(body).results;
-  const moviesWithWedgeAntilles = films.filter(film =>
-    film.characters.includes(`people/${characterId}`)
-  );
-
-  console.log(moviesWithWedgeAntilles.length);
 });
