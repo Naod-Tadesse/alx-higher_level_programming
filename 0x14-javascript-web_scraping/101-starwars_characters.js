0072b890a97a9d36f95a477b117114275b817f37
Naod Taddese <naodtds@gmail.com>
2024-03-26 21:39:39 +0300

    done

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,26 +1,45 @@
 #!/usr/bin/node
 const request = require('request');
 
-const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-request.get(url, (error, response, body) => {
+request.get(apiUrl, (error, response, body) => {
   if (error) {
-    console.log(error);
+    console.error(error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error(`Failed to fetch movie data: ${response.statusCode}`);
+    return;
   }
 
   const movie = JSON.parse(body);
   const characters = movie.characters;
 
-  const getCharacter = (characterUrl) => {
-    request.get(characterUrl, (cError, cResponse, cBody) => {
-      if (cError) {
-        console.error(cError);
+  const printCharacters = (index) => {
+    if (index === characters.length) {
+      return;
+    }
+
+    const characterUrl = characters[index];
+    request.get(characterUrl, (charError, charResponse, charBody) => {
+      if (charError) {
+        console.error(charError);
+        return;
       }
 
-      const cha = JSON.parse(cBody);
-      console.log(cha.name);
+      if (charResponse.statusCode !== 200) {
+        console.error(`Failed to fetch character data: ${charResponse.statusCode}`);
+        return;
+      }
+
+      const character = JSON.parse(charBody);
+      console.log(character.name);
+      printCharacters(index + 1);
     });
   };
 
-  characters.forEach(getCharacter);
+  printCharacters(0);
 });
